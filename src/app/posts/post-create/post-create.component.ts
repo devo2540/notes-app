d820b367ba9adf1ac319c367f2f7e29684b38832
Devo <the.devin.carter@outlook.com>
2018-08-14 19:03:41 -0500

    add action bar to expanded post. TODO: add functionality to edit/delete buttons

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
   onAddPost(form: NgForm) {
     if(form.invalid) {return}
     this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 
   constructor(public postsService: PostsService) { }
