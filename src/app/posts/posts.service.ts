3ddf6b6481cb96e8056ce6203c18ca85f8064688
Devo <the.devin.carter@outlook.com>
2018-08-16 20:17:40 -0500

    wire up delete button

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
-    private posts: Post[] = []
+    private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
     constructor(private http: HttpClient) { }
 
     getPosts() {
-        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-            .subscribe((postData) => {
-                this.posts = postData.posts;
+        this.http
+            .get<{ message: string; posts: any }>(
+                'http://localhost:3000/api/posts'
+            )
+            .pipe(map((postData) => {
+                return postData.posts.map(post => {
+                    return {
+                        title: post.title,
+                        content: post.content,
+                        id: post._id
+                    };
+                });
+            }))
+            .subscribe(transformedPosts => {
+                this.posts = transformedPosts;
                 this.postsUpdated.next([...this.posts]);
             }); 
     }
@@ -30,6 +43,13 @@ export class PostsService {
                 console.log(responseData);
                 this.posts.push(post);
                 this.postsUpdated.next([...this.posts]); // emit new value (copy of posts after updated)
+            });
+    }
+
+    deletePost(postId: string) {
+        this.http.delete('http://localhost:3000/api/posts/' + postId)
+            .subscribe(() => {
+                console.log(postId + ' Deleted!');
             });
     }
 }
