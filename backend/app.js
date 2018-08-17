e0b2ffc22d5e51ab6a139d6fc73186534b4d743f
Devo <the.devin.carter@outlook.com>
2018-08-16 19:57:05 -0500

    add middleware to fetch posts from database

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     next();
 });
 
+// save to database
 app.post('/api/posts', (req, res, next) => {
     const post = new Post({
         title: req.body.title,
@@ -38,23 +39,14 @@ app.post('/api/posts', (req, res, next) => {
     });
 });
 
+// fetch from database
 app.get('/api/posts', (req, res, next) => {
-    const posts = [
-        {
-            id: 'aaa123',
-            title: 'first server side post',
-            content: 'this is coming from the server'
-        },
-        {
-            id: 'bbb456',
-            title: 'second server side post',
-            content: 'this is also coming from the server!'
-        }
-    ];
-    
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
+    // find ALL posts in database
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Posts fetched successfully!',
+            posts: documents
+        });
     });
 });
 
