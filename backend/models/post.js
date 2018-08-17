9b32fce8c0bad28fd0a5857c9c88739ee3e4df77
Devo <the.devin.carter@outlook.com>
2018-08-16 19:18:57 -0500

    create mongoose schema/model

--- backend/models/post.js
+++ backend/models/post.js
@@ -6,3 +6,5 @@ var postSchema = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true }
 });
+
+module.exports = mongoose.model('Post', postSchema);
